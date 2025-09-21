a2adfa0c831d82b275ce1962ea44a73d99045ba4
hashini <cb015618@students.apiit.lk>
2025-09-21 19:58:41 +0530

    Initial commit - HTML website

--- script.js
+++ script.js
@@ -388,26 +388,9 @@ document.addEventListener("DOMContentLoaded", () => {
       // Toggle the clicked one
       faq.classList.toggle("active");
     });
-
-    
   });
 });
-// --- Modal close button ---
-const modal = document.getElementById("recipeModal");
-const modalClose = document.getElementById("closeModal");
 
-// Close modal when close button is clicked
-if (modalClose) {
-  modalClose.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-  // Close modal when clicking outside the modal content
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
-});
-}
 //mindfulness page
 
  
